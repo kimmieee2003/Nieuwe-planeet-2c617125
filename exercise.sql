a485c3b871d59ac10532df2bb1782688e941f27a
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-15 10:25:34 +0200

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,11 +1,12 @@
 /*CREATE DATABASE sterrenstelsel;*/
 USE sterrenstelsel;
 /*CREATE TABLE planeten (naam VARCHAR(20));*/
-/*insert into planeten (naam)  value ("Zon"), ("Mercurius"), ("Venus"), ("Aarde"), ("Mars");*/
+/*insert into planeten (naam)  value ("Zon"), ("Mercurius"), ("Venus"), ("Aarde"), ("Mars"), ("Mars");*/
+/*insert into planeten (naam) value ("Mars");*/
 /*SELECT * FROM planeten;*/
 TRUNCATE TABLE  planeten;
 
-ALTER TABLE planeten ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+-- ALTER TABLE planeten ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
 ALTER TABLE planeten MODIFY COLUMN diameter INT;
 ALTER TABLE planeten MODIFY COLUMN afstand_tot_de_zon INT;
 ALTER TABLE planeten MODIFY COLUMN massa INT;
@@ -14,5 +15,6 @@ INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) V
 INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mercurius", 57.910000, 0.1, 4.880, NULL);
 INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Venus", 108.208930, 0.9, 12.104, "1962-03-01");
 INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Aarde", 149.597870, 1, 12.756, NULL);
+INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mars", 227.936640, 0.1, 6.794, NULL);
 INSERT INTO planeten (naam, afstand_tot_de_zon, massa, diameter, bezoek_datum) VALUE ("Mars", 227.936640, 0.1, 6.794, NULL);
 SELECT * FROM planeten;
